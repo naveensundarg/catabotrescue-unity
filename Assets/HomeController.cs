a498433c2005dd9d11729f80fa3e414a451a612b
Naveen Sundar Govindarajulu <naveensundarg@gmail.com>
2014-02-23 19:22:04 -0500

    no message

--- Assets/HomeController.cs
+++ Assets/HomeController.cs
@@ -30,11 +30,11 @@ public class HomeController : MonoBehaviour {
 	void OnGUI() {
 		GUI.skin.label.normal.textColor = Color.black;
 		GUI.skin.label.fontStyle=FontStyle.Bold;
-
+		GUI.skin.textField.stretchHeight = true;//TextAnchor.MiddleCenter
 		GUI.skin.label.normal.textColor = Color.black;
 		GUI.skin.label.hover.textColor = Color.gray;
 		GUI.Label (new Rect (Screen.width / 2-100, Screen.height / 2, 95, 30), "Login(Email)");
-		email = GUI.TextField(new Rect(Screen.width/2, Screen.height/2, 200, 30), email, 50);
+		email = GUI.TextField(new Rect(Screen.width/2, Screen.height/2, 200, 25), email, 50);
 
 		if(GUI.Button(new Rect(Screen.width/2+210, Screen.height/2, 50, 30), "Go")){
 
